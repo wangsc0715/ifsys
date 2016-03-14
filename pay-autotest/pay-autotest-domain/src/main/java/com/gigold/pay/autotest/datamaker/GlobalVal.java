72a13f88e3307f16d31cdaf460ce9b24102197ee
chenkuan <chenkuan@gigold.com>
2016-03-14 11:03:48 +0800

    数据生成功能

---
+++ pay-autotest/pay-autotest-domain/src/main/java/com/gigold/pay/autotest/datamaker/GlobalVal.java
@@ -0,0 +1,13 @@
+package com.gigold.pay.autotest.datamaker;
+
+/**
+ * Created by chenkuan
+ * on 16/3/7.
+ * 全局变量
+ */
+public class GlobalVal {
+    public static PropertiesPhaser globalVal = new PropertiesPhaser("/pay-autotest-script/src/main/resources/server.properties");
+    public static String get(String key){
+        return globalVal.get(key);
+    }
+}
---
+++ pay-autotest/pay-autotest-domain/src/main/java/com/gigold/pay/autotest/datamaker/PropertiesPhaser.java
@@ -0,0 +1,41 @@
+package com.gigold.pay.autotest.datamaker;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+/**
+ * Created by chenkuan
+ * on 16/3/7.
+ */
+public class PropertiesPhaser {
+    private Properties p = new Properties();
+
+    public PropertiesPhaser(String dir){
+        // 生成文件对象
+        File pf = new File(System.getProperty("user.dir")
+                + "/"+dir);
+
+        // 生成文件输入流
+        FileInputStream inpf = null;
+        try {
+            inpf = new FileInputStream(pf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 生成properties对象
+
+        try {
+            p.load(inpf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
+
+
+
+    public String get(String Key){
+        return this.p.getProperty(Key);
+    };
+}
